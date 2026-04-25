2457385736e5862122e38ba4c0b106cab2167a1c
theo-al <emailtheoprauerj@gmail.com>
2026-04-25 14:16:11 -0300

    WIP: formatação
    _ e etc

--- src/SumoIR.h
+++ src/SumoIR.h
@@ -33,10 +33,10 @@ class SumoIR {
 
     bool _change = false;
     bool _available = false;
-    int command = -1;
+    int _command = -1;
 
   public:
-    enum {
+    enum SumoState {
         SUMO_STOP = 0,
         SUMO_PREPARE,
         SUMO_START
@@ -45,8 +45,8 @@ class SumoIR {
     IRrecv IR_IN = IRrecv(_pin); // Receptor
 
     // Begin
-    void begin(){ begin(_pin); }
-    void begin(uint8_t pin){
+    void begin() { begin(_pin); }
+    void begin(uint8_t pin) {
         if (_pin != pin) {
             _pin = pin;
             IR_IN.setReceivePin(_pin);
@@ -56,28 +56,28 @@ class SumoIR {
     }
 
     bool available() { return _available; }
-    int  read()      { return command;   }
+    int  read()      { return _command;   }
 
     // Led
-    void setLed (int pin, bool state_on, uint16_t dt) {
+    void setLed(int pin, bool state_on, uint16_t dt) {
       LED = pin;
       LED_state_on = state_on;
       LED_dt = dt;
       if (LED >= 0) {
-        pinMode( LED, OUTPUT );
-        digitalWrite( LED, !LED_state_on );
+        pinMode(LED, OUTPUT);
+        digitalWrite(LED, !LED_state_on);
       }
     }
 
-    void updateLed(){
+    void updateLed() {
       if (LED < 0) return;
 
-      if (_mode == SUMO_STOP) {
+      if        (_mode == SUMO_STOP) {
         digitalWrite(LED,!LED_state_on);
       } else if (_mode == SUMO_START) {
         digitalWrite(LED,LED_state_on);
       } else if (_mode == SUMO_PREPARE) {
-        if (command == 1) {
+        if (_command == 1) {
           digitalWrite(LED,LED_state_on);
           LED_timeout = millis() + LED_dt;
         } else if (millis() >= LED_timeout) {
@@ -90,7 +90,7 @@ class SumoIR {
     ////////////////////////////// IR INPUT /////////////////////////////////////
     int update() {
       _available = false;
-      command = -1;
+      _command = -1;
       _change = false;
 
       if (IR_IN.decode()){
@@ -106,47 +106,55 @@ class SumoIR {
 
         if (IN_protocol == SONY) {
           switch (IN_cmd) {
-            case 0:  command = 1; if (_mode == SUMO_STOP   ) _mode = SUMO_PREPARE; break; // PREPARE
-            case 1:  command = 2; if (_mode == SUMO_PREPARE) _mode = SUMO_START;   break; // START
-            case 2:  command = 3; _mode = SUMO_STOP; break; // STOP
-            default: command = IN_cmd + 1; break;
+            // PREPARE
+            case 0:  _command = 1; if (_mode == SUMO_STOP   ) _mode = SUMO_PREPARE; break;
+            // START
+            case 1:  _command = 2; if (_mode == SUMO_PREPARE) _mode = SUMO_START;   break;
+            // STOP
+            case 2:  _command = 3; _mode = SUMO_STOP; break;
+            // EXTRAS
+            default: _command = IN_cmd + 1; break;
           }
         } else if (IN_protocol == SAMSUNG) {
           switch (IN_cmd) {
-            case 4:   command = 1; if(_mode == SUMO_STOP   ) _mode = SUMO_PREPARE; break; // PREPARE
-            case 5:   command = 2; if(_mode == SUMO_PREPARE) _mode = SUMO_START;   break; // START
-            case 6:   command = 3; _mode = SUMO_STOP; break; // STOP
-            case 8:   command = 4; break;
-            case 9:   command = 5; break;
-            case 10:  command = 6; break;
-            case 12:  command = 7; break;
-            case 13:  command = 8; break;
-            case 14:  command = 9; break;
-            case 17:  command = 0; break;
-            case 96:  command = 10; break; // Up
-            case 97:  command = 11; break; // Down
-            case 98:  command = 12; break; // Rigth
-            case 101: command = 13; break; // Left
-            case 104: command = 14; break; // OK
-            case 108: command = 15; break; // A
-            case 20:  command = 16; break; // B
-            case 21:  command = 17; break; // C
-            case 22:  command = 18; break; // D
-            case 224: command = 19; break; // STB
-            case 2  : command = 20; break; // ON
+            // PREPARE
+            case 4:   _command = 1; if (_mode == SUMO_STOP   ) _mode = SUMO_PREPARE; break;
+            // START
+            case 5:   _command = 2; if (_mode == SUMO_PREPARE) _mode = SUMO_START;   break;
+            // STOP
+            case 6:   _command = 3; _mode = SUMO_STOP; break;
+            // EXTRAS
+            case 8:   _command = 4; break;
+            case 9:   _command = 5; break;
+            case 10:  _command = 6; break;
+            case 12:  _command = 7; break;
+            case 13:  _command = 8; break;
+            case 14:  _command = 9; break;
+            case 17:  _command = 0; break;
+            case 96:  _command = 10; break; // Up
+            case 97:  _command = 11; break; // Down
+            case 98:  _command = 12; break; // Rigth
+            case 101: _command = 13; break; // Left
+            case 104: _command = 14; break; // OK
+            case 108: _command = 15; break; // A
+            case 20:  _command = 16; break; // B
+            case 21:  _command = 17; break; // C
+            case 22:  _command = 18; break; // D
+            case 224: _command = 19; break; // STB
+            case 2  : _command = 20; break; // ON
           }
         }
         _change = (mode_before != _mode);
         logif();
       }
       updateLed();
-      return command;
+      return _command;
     }
 
     // return protocol
     decode_type_t protocol() { return IN_protocol; }
-    String protocol_str()    { return protocol_str( IN_protocol ); }
-    String protocol_str(decode_type_t p) {
+    char* protocol_str()     { return protocol_str(IN_protocol); }
+    char* protocol_str(decode_type_t p) {
       switch (p) {
         case NEC:       return "NEC"      ; break;
         case SONY:      return "SONY"     ; break;
@@ -175,14 +183,15 @@ class SumoIR {
     void logif(){ if (DEBUG) log(); }
     void log()  { Serial.println(str()); }
 
-    String str() {
-      char buf[100];
-      sprintf( buf, ">> [%s] IR CMD: %d [ 0x%.8X ] [ %s ]\n", mode_str(), command, IN_data, protocol_str().c_str() );
+    char* str() {
+      static char buf[100];
+      sprintf(buf, ">> [%s] IR CMD: %d [ 0x%.8X ] [ %s ]\n",
+                    mode_str(), _command, IN_data, protocol_str());
       return buf;
     }
 
     // Sumo modes    
-    String mode_str() {
+    char* mode_str() {
       switch (_mode) {
         case SUMO_START:   return "START";   break;
         case SUMO_STOP:    return "STOP";    break;
@@ -198,9 +207,9 @@ class SumoIR {
 
     bool change() { return _change; }
     bool start()  { return (_mode == SUMO_START) && _change; }
-    bool stop()   { return (_mode == SUMO_STOP) && _change; }
+    bool stop()   { return (_mode == SUMO_STOP)  && _change; }
     bool on()     { return _mode == SUMO_START; }
-    bool off()    { return _mode == SUMO_STOP; }
+    bool off()    { return _mode == SUMO_STOP;  }
     bool prepare(){ return _mode == SUMO_PREPARE; }
 };
 
